d58011b4beecbde322ec7900ca4dab8806285cbf
Edgar Ramírez <edgar.ramirez.fuentes.dev@gmail.com>
2023-05-14 19:55:35 -0600

    fix:Bug in the admin user creation

--- api/core/models/User.py
+++ api/core/models/User.py
@@ -54,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_login = models.DateTimeField(null=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
